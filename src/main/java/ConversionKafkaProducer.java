c8d9574cd08035f1fc370dd21b265ac928afe720
anshumankumar <anshu266man@gmail.com>
2017-05-30 16:17:58 +0530

    Updateing the Kafka Producer

--- src/main/java/ConversionKafkaProducer.java
+++ src/main/java/ConversionKafkaProducer.java
@@ -7,19 +7,26 @@ import java.util.Properties;
 /**
  * Created by anshuman on 30/5/17.
  */
-public class ConversionKafkaProducer {
-    Producer<String, String> producer;
-
-    ConversionKafkaProducer() {
-        Properties props = new Properties();
+public class ConversionKafkaProducer  implements java.io.Serializable{
+     transient private Producer<String, String> producer;
+    
+    Producer<String,String> getProducer()
+    {
+	if (producer ==null)
+	{
+       	Properties props = new Properties();
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("producer.type", "async");
         props.put("metadata.broker.list", "192.169.34.61:9092,192.169.34.62:9092,192.169.34.63:9092");
+        props.put("bootstrap.servers", "192.169.34.61:9092,192.169.34.62:9092,192.169.34.63:9092");
         producer = new KafkaProducer<String, String>(props);
+
+	}
+	return producer;
     }
 
     void send(String key, String value) {
-        producer.send(new ProducerRecord<String, String>("AdConverted",key, value));
+        getProducer().send(new ProducerRecord<String, String>("AdConverted",key, key+":" + value));
     }
 }
